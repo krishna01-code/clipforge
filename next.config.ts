072af5660c7e500937750ead3502c785f7ffa454
Krishna Singh <trackmode.cs@gmail.com>
2026-03-28 13:16:58 +0530

    fix: app router body size limit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: "50mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
     },
-    responseLimit: "50mb",
   },
 };
 
